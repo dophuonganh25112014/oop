6e2310ad86b0cf47fc45c48c54749c13e802e0cc
Do Phuong Anh <25112014@sr.vju.ac.vn>
2026-04-08 21:51:03 +0700

    up oop6code.py

--- oop6code.py
+++ oop6code.py
@@ -29,14 +29,16 @@ class HangHoa(ABC):
     def __eq__(self,o):return self.__ma==o.__ma 
     def __lt__(self,o): return self.__gia<o.__gia 
     def __hash__(self,o): return hash(self.__ma)
-    
+    def __repr__(self):
+        return (f"{self.__class__.__name__}("
+            f"ma='{self.__ma}',"
+            f"ten='{self.__ten}',"
+            f"gia={self.__gia})") 
 class HangSanhSu(HangHoa):
     def __init__(self, ma, ten, nsx, gia, loai_nguyenlieu):
         self.loai_nguyenlieu = loai_nguyenlieu 
         super().__init__(ma,ten,nsx,gia)
-    def loai_nguyenlieu(self): return "Gốm Sứ Bát Tràng"
-    def loai_hang(self):
-        return "Hàng Sành Sứ"
+    def loai_hang(self): return "Gốm Sứ Bát Tràng"
     def inTTin(self):
         return (f"{super().inTTin()}"
                 f" | NL:{self.loai_nguyenlieu}")
@@ -49,15 +51,15 @@ class HangThucPham(HangHoa):
     def ngay_hethan(self): return "3 tháng sau khi mua"
     def inTTin(self):
         return (f"{super().inTTin()}"
-                f" | NSX :{self.ngay_sx}"
-                f" | NHH :{self.ngay_hethan}")
+                f" | NSX :{self.ngay_sx()}"
+                f" | NHH :{self.ngay_hethan ()}")
 
 
 
 class HangDienMay(HangHoa):
     def __init__(self,ma,ten,nsx,gia,bhanh,dap,cs):
         super().__init__(ma,ten,nsx,gia)
-        self.__bhanh , self.__dap , self.__cs = bhanh , dap ,cs
+        self.__bhanh , self.__dap , self.__cs = bhanh , dap ,cs 
     def loai_hang(self):return "Điện Máy"
     def inTTin(self):
         return (f"{super().inTTin()}"
@@ -70,7 +72,7 @@ ds = [HangDienMay ("111", "Máy Lạnh " , "Panasonic", 12000000 ,24,222,150) , 
 for sp in sorted (ds):
     print(sp)
     
-with open("kho.txt" , "w") as f:
+with open("kho.txt" , "w", encoding ="utf -8") as f:
     for sp in ds:
         f.write(repr(sp)+ "\n")
         
